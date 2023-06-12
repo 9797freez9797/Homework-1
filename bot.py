184e00888eec8c6a8f68361016a2bc511f69c7b7
9797FREEZ9797 <valeriypotapov2006@gmail.com>
2023-06-12 10:37:30 +0300

    Update

--- bot.py
+++ bot.py
@@ -7,25 +7,20 @@ from decouple import config
 
 API_TOKEN = config('TELEGRAM_BOT_TOKEN')
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
-# Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# Определение функции для генерации клавиатуры
 def generate_keyboard(examples):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for example in examples:
         keyboard.add(example)
     return keyboard
 
-# Глобальные переменные
 examples = []
 expected_result = None
 
-# Обработчик команды /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     global expected_result, examples
@@ -33,7 +28,6 @@ async def send_welcome(message: types.Message):
     text = 'Привет!\n'
     operations = ['+', '-', '*', '/']
 
-    # Генерация 4 случайных примеров
     for i in range(4):  
         mathematical_operation1 = random.choice(operations)
         mathematical_operation2 = random.choice(operations)
@@ -43,15 +37,14 @@ async def send_welcome(message: types.Message):
         example = f"{number1} {mathematical_operation1} {number2} {mathematical_operation2} {number3}"
         examples.append(example)
     
-    random_example = random.choice(examples)  # Выбор случайного примера
+    random_example = random.choice(examples)
 
-    expected_result = eval(random_example)  # Определение ожидаемого результата примера
-    text = f"Выбери пример, в котором результат равен {expected_result}"
+    expected_result = eval(random_example)
+    text = f"Выберите пример, в котором результат равен {expected_result}"
 
     keyboard = generate_keyboard(examples)
     await message.reply(text, reply_markup=keyboard)
 
-# Обработчик ответов пользователя
 @dp.message_handler(lambda message: message.text in examples)
 async def check_answer(message: types.Message):
     global expected_result
